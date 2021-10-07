91c506327178c7fca5bc72e6bb1a227e90f6121b
swechchha11 <85487603+swechchha11@users.noreply.github.com>
2021-10-07 16:21:40 +0530

    Add files via upload

--- backend_day11/app.js
+++ backend_day11/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const router = require('./routes/index');
 
 var app = express();
 
@@ -38,10 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-
-
-router.post('/register', )
 
 module.exports = app;
